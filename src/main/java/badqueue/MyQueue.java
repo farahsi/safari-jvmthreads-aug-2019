bd11457922b3086d9d968433e08a1d8c3d05406b
Simon Roberts <simon@dancingcloudservices.com>
2019-08-02 12:43:50 -0600

    Bad Queue Fixed!

--- src/main/java/badqueue/MyQueue.java
+++ src/main/java/badqueue/MyQueue.java
@@ -4,41 +4,41 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 public class MyQueue<T> {
-//  public static final int SIZE = 10;
-//  private T[] data = (T[]) new Object[SIZE];
-//  private int count = 0;
-//
-//  public void put(T t) throws InterruptedException {
-//    synchronized (this) {
-//      while (count >= 10)
-//        this.wait(); // releases lock (must be transactionally correct!)
-//      // does not restart executing until RECOVERS the lock...
-//
-//      data[count++] = t; // needs "atomicity" or mutual exclusion
-//      this.notify();
-//    }
-//  }
-//
-//  public T take() throws InterruptedException {
-//    synchronized (this) {
-//      while (count <= 0)
-//        this.wait();
-//      this.notify(); // Confusing, but SAFE position!!!
-//      T item = data[0];
-//      count--;
-//      System.arraycopy(item, 1, item, 0, count - 1);
-//    }
-//
-//    return item;
-//  }
-//
-//}
+  public static final int SIZE = 10;
+  private T[] data = (T[]) new Object[SIZE];
+  private int count = 0;
+
+  public void put(T t) throws InterruptedException {
+    synchronized (this) {
+      while (count == SIZE)
+        this.wait(); // releases lock (must be transactionally correct!)
+      // does not restart executing until RECOVERS the lock...
+
+      data[count++] = t; // needs "atomicity" or mutual exclusion
+      this.notify();
+    }
+  }
+
+  public T take() throws InterruptedException {
+    synchronized (this) {
+      while (count == 0)
+        this.wait();
+      this.notify(); // Confusing, but SAFE position!!!
+      T item = data[0];
+      // Error was that I had passed "item" instead of "data" doh!?!?
+      // item isn't even an array, but the signature of System.arraycopy takes Object, not "array"
+      // (which is odd and news to me!)
+      System.arraycopy(data, 1, data, 0, --count);
+      return item;
+    }
+  }
+
 
   // "producer-consumer" model is now (mostly) called Actors
 
   public static void main(String[] args) {
-//    MyQueue<int[]> mq = new MyQueue<>();
-    BlockingQueue<int[]> mq = new ArrayBlockingQueue<>(10);
+    MyQueue<int[]> mq = new MyQueue<>();
+//    BlockingQueue<int[]> mq = new ArrayBlockingQueue<>(10);
     Runnable prod = () -> {
       try {
         for (int i = 0; i < 10_000; i++) {
